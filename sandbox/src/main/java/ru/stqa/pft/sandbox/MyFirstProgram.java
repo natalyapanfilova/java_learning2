c3fce1c372ceb1125d6ab128021918ef8223c61a
natalyapanfilova <natalyazhilova@yandex.ru>
2019-08-05 12:43:26 +0300

    Добавлены методы в новые объекты

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,21 +8,13 @@ public class MyFirstProgram {
 		hello("Natalya");
 
 		Square s = new Square(6);
-		System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
 		Rectangle r = new Rectangle(4, 5);
-		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 
 	public static void hello(String somebody) {
 		System.out.println ("Hello, " + somebody + "!");
-	}
-
-	public static double area(Square s) {
-		return s.l * s.l;
-	}
-
-	public static double area(Rectangle r) {
-		return r.a * r.b;
 	}
 }
